930fc1c8a1c51642d3ce193d443212bbba4dda61
gahbriehel <babatise002@gmail.com>
2024-12-18 05:33:33 +0100

    set up project || created react-app and installed dependencies

---
+++ openlinks/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="temp">
+      <img src='images/profile.png' alt='logo' height={200}></img>
+      <h1>Open your links</h1>
+    </div>
+  );
+}
+
+export default App;
