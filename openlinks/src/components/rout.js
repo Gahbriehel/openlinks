dd310e2324c1d980a3b840f91d18e02bc10546ec
gahbriehel <babatise002@gmail.com>
2024-12-23 14:27:53 +0100

    created the route to about page

--- openlinks/src/components/rout.js
+++ openlinks/src/components/rout.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Routes, Route} from 'react-router'
 import Home from './home'
+import About from './about'
 
 const Rout = () => {
   return (
     <>
     <Routes>
       <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
     </Routes>
     </>
   )
