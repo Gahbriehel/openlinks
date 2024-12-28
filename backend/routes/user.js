cbd5c49c565504b86a3ac789b55cb487a331abc9
Donald <donumeh54@gmail.com>
2024-12-28 13:53:58 +0100

    updated backend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,19 +2,31 @@
 const { User } = require('../models/user');
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
 
 router.get(`/`, async (req, res) => {
-    const users = await User.find();
+    const users = await User.find().select("-passwordHash");
 
     if (!users) {
         res.status(500).json({ success: false })
     }
 
     res.json({ success: "ok", statusCode: 200, users })
-})
+});
+
+router.get('/:id', async (req, res) => {
+    const user = await User.findById(req.params.id).select("-passwordHash");
+
+    if (!user) {
+        res.status(500).json({ success: false, message: "The user was not found" })
+    }
+    res.status(200).json({ success: true, user });
+});
 
 router.post(`/`, async (req, res) => {
     let user = new User({
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
         ...req.body
     });
 
