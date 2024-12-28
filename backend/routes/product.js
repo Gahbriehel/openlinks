c2d0e51b7eab7b9ee26f3c56f75e2614aebb2d85
Donald <donumeh54@gmail.com>
2024-12-28 12:32:30 +0100

    updated backend

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,5 +1,5 @@
 // express and api router for open_link
-const { Category } = require('../models/category');
+// const { Category } = require('../models/category');
 const { Product } = require('../models/product');
 const express = require('express');
 const router = express.Router();
@@ -14,12 +14,44 @@ router.get(`/`, async (req, res) => {
     res.json({ success: "ok", statusCode: 200, products })
 })
 
+router.get('/:id', async (req, res) => {
+    const product = await Product.findById(req.params.id).populate('category');
+
+    if (!product) {
+        res.status(500).json({ success: false, message: "product cannot be found" })
+    }
+
+    res.json({ success: "ok", statusCode: 200, product })
+})
+
+router.put('/:id', async (req, res) => {
+    const product = await Product.findByIdAndUpdate(
+        req.params.id,
+        { ...req.body },
+        { new: true }
+    )
+
+    if (!product) {
+        return res.status(404).send("product couldn't be updated")
+    }
+    res.send({ success: "ok", status: 201, product })
+
+})
+
+router.delete('/:id', async (req, res) => {
+    const product = await Product.findByIdAndDelete(req.params.id);
+
+    if (!product) {
+        return res.status(404).json({ success: false, message: "category not found" });
+    }
+    return res.status(200).json({ success: true, messag: "category has been deleted" })
+})
+
 router.post(`/`, async (req, res) => {
     const category = await Category.findById(req.body.category);
     if (!category) {
         return res.status(400).json({ success: false, message: "Invalid Category" })
     }
-    console.log("Here 2")
     let product = new Product({
         name: req.body.name,
         description: req.body.description,
@@ -33,12 +65,10 @@ router.post(`/`, async (req, res) => {
         dateCreated: req.body.dateCreated
     })
     product = await product.save();
-    console.log("Here")
 
     if (!product) {
         return res.status(500).json({ success: false, message: "product cannot be created" })
     }
-    console.log("Here 3")
     return res.status(201).json({ success: "ok", status: 200, product })
 })
 
