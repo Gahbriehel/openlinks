c0bf3d37da655807474f65b0a7b22c2f1a355d1a
gahbriehel <babatise002@gmail.com>
2024-12-29 17:52:18 +0100

    eventual form component

--- openlinks/src/components/contactForm.js
+++ openlinks/src/components/contactForm.js
@@ -1,18 +1,57 @@
-import {useState} from 'react';
+import { useState } from "react";
 
-export default function  ControlledComponent()  {
-    const  [inputValue, setInputValue] =  useState('');
+export default function SimpleForm() {
+    const [email, setEmail] = useState('');
+    const [error, setError] = useState('');
 
-    const  handleChange = (event) => {
-        setInputValue(event.target.value);
-    };
+    const handleChange = (event) => {
+        setEmail(event.target.value);
+        if (event.target.value) {
+            setError("")
+        }
+    }
 
-return  (
-<form>
-    <label>Input Value:
-    <input  type="text"  value={inputValue} onChange={handleChange} />
-    </label>
-    <p>Input Value: {inputValue}</p>
-    </form>
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        if (!email) {
+            setError("Email is required!");
+        } else {
+            alert(`Submitted email: ${email}`)
+        }
+    }
 
-)};
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>Input email: </label>
+            <input type="email" value={email} onChange={handleChange}></input>
+            <p>Here's what you've typed: {email}</p>
+            <p style={{ color: "red" }}>{error}</p>
+            <button type="submit">Submit here</button>
+        </form>
+    )
+}
+
+
+
+// import {useState} from 'react';
+
+// export default function  SimpleForm()  {
+//     const  [email, setEmail] =  useState('');
+
+//     const  handleChange = (event) => {
+//         setEmail(event.target.value);
+//     };
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault()
+//     }
+
+// return  (
+// <form onSubmit={handleSubmit}>
+//     <label>Input email:
+//     <input  type="email"  value={email} onChange={handleChange} />
+//     </label>
+//     <p>Typed email: {email}</p>
+//     <button type='submit'>Submit here</button>
+//     </form>
+// )};
