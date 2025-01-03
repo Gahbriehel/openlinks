f9330f206d634b685b10a1cf0bc6c59fe42ffb7f
gahbriehel <babatise002@gmail.com>
2025-01-03 12:49:53 +0100

    refactored navigation structure by creating a separate NavBottom component for improved organization

--- openlinks/src/components/nav.js
+++ openlinks/src/components/nav.js
@@ -2,10 +2,9 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import './nav.css'
 import { FaSearch } from 'react-icons/fa';
-import { IoIosLaptop, IoMdPricetag, IoMdHome } from "react-icons/io";
-import { MdOutlineCategory, MdPersonOutline, MdOutlineShoppingCart, MdFilterAlt } from "react-icons/md";
-import { ImPriceTags } from "react-icons/im";
-import { RiArrowDropDownLine } from "react-icons/ri";
+import { MdPersonOutline, MdOutlineShoppingCart } from "react-icons/md";
+import NavBottom from './navBottom';
+
 // import { FaRankingStar } from "react-icons/fa6";
 
 
@@ -34,18 +33,7 @@ const Nav = () => {
                     </div>
                 </div>
                 <hr></hr>
-                <div className='nav_bottom'>
-                    <ul>
-                        <li><Link to='/' className='link'><IoMdHome />Home</Link></li>
-                        <li><Link to='/brands' className='link'><IoIosLaptop /> Laptop Brands</Link></li>
-                        <li><Link to='/categories' className='link'><MdOutlineCategory /> Categories  <RiArrowDropDownLine /></Link></li>
-                        <li><Link to='/price_range' className='link'><ImPriceTags /> Price Range  <RiArrowDropDownLine /></Link></li>
-                        <li><Link to='/specific' className='link'><MdFilterAlt /> Specific  <RiArrowDropDownLine /></Link></li>
-                        <li><Link to='/sale' className='link'><IoMdPricetag /> SALE</Link></li>
-                        <li><Link to='/about' className='link'>About</Link></li>
-                        <li><Link to='/contact' className='link'>Contact Us</Link></li>
-                    </ul>
-                </div>
+               <NavBottom />
             </div>
         </>
     )
---
+++ openlinks/src/components/navBottom.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { IoIosLaptop, IoMdPricetag, IoMdHome } from "react-icons/io";
+import { MdOutlineCategory, MdFilterAlt } from "react-icons/md";
+import { ImPriceTags } from "react-icons/im";
+import { RiArrowDropDownLine } from "react-icons/ri";
+
+const NavBottom = () => {
+  return (
+    <div className='nav_bottom'>
+    <ul>
+        <li><Link to='/' className='link'><IoMdHome />Home</Link></li>
+        <li><Link to='/brands' className='link'><IoIosLaptop /> Laptop Brands</Link></li>
+        <li><Link to='/categories' className='link'><MdOutlineCategory /> Categories  <RiArrowDropDownLine /></Link></li>
+        <li><Link to='/price_range' className='link'><ImPriceTags /> Price Range  <RiArrowDropDownLine /></Link></li>
+        <li><Link to='/specific' className='link'><MdFilterAlt /> Specific  <RiArrowDropDownLine /></Link></li>
+        <li><Link to='/sale' className='link'><IoMdPricetag /> SALE</Link></li>
+        <li><Link to='/about' className='link'>About</Link></li>
+        <li><Link to='/contact' className='link'>Contact Us</Link></li>
+    </ul>
+</div>
+  )
+}
+
+export default NavBottom
