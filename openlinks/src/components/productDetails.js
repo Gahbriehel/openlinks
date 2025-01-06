8f08a64f0488d224718ccd8bfa1db329d8f99352
gahbriehel <babatise002@gmail.com>
2025-01-07 00:37:06 +0100

    style: updated layout and styling for ProductDetails component, improved button design and added new sections

--- openlinks/src/components/productDetails.js
+++ openlinks/src/components/productDetails.js
@@ -3,6 +3,8 @@ import { useDispatch } from 'react-redux';
 import { addToCart } from './redux/cart/cartSlice';
 import './productDetails.css'
 import { FaArrowLeft } from "react-icons/fa";
+import { MdOutlineShoppingCart } from "react-icons/md";
+
 
 
 const ProductDetails = ({ product, backToList }) => {
@@ -17,13 +19,17 @@ const ProductDetails = ({ product, backToList }) => {
                     <img src={product.image} alt='laptop'></img>
                 </div>
                 <div className='productDetails_container_right'>
-                    <h2>{product.name}</h2>
-                    <p>{product.brand} {product.category}</p>
-
-                    <h3>Product Details</h3>
-                    <p>{product.longDescription}</p>
-                    <p>${new Intl.NumberFormat('en-us').format(product.price)}</p>
-                    <button style={{ cursor: 'pointer' }} onClick={() => dispatch(addToCart(product))}>Add to Cart</button>
+                    <div className='product_details_section1'>
+                        <h2>{product.name}</h2>
+                        <p>{product.brand}</p>
+                        <p>Category: {product.category}</p>
+                    </div>
+                    {/* <h3>Product Details</h3> */}
+                    <div className='product_details_section2'>
+                        <p>{product.longDescription}</p>
+                        <p className='price_p'>Price: ${new Intl.NumberFormat('en-us').format(product.price)}</p>
+                    </div>
+                    <button style={{ cursor: 'pointer' }} onClick={() => dispatch(addToCart(product))}><MdOutlineShoppingCart /> Add to Cart</button>
                 </div>
             </div>
         </div>
