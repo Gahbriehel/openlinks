1cb26ac119dae55ea1ad22fac2b28b4ff53e576b
gahbriehel <babatise002@gmail.com>
2025-01-08 17:57:29 +0100

    feat: added Profile route to the routing component

--- openlinks/src/components/rout.js
+++ openlinks/src/components/rout.js
@@ -7,6 +7,7 @@ import Wholesale from './wholesale'
 import Cart from './cart'
 import Signup from './signup'
 import Login from './login'
+import Profile from './profile'
 
 const Rout = () => {
   return (
@@ -20,6 +21,7 @@ const Rout = () => {
       <Route path='/cart' element={<Cart/>}/>
       <Route path='/signup' element={<Signup/>}/>
       <Route path='/login' element={<Login/>}/>
+      <Route path='/profile' element={<Profile/>}/>
     </Routes>
     </>
   )
