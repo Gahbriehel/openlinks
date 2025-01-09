86731b3cfccaf44ce6d778efa7601ca393e848f0
gahbriehel <babatise002@gmail.com>
2025-01-09 07:16:27 +0100

    feat: implemented user account dropdown in navigation with logout functionality

--- openlinks/src/components/nav.js
+++ openlinks/src/components/nav.js
@@ -2,13 +2,19 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import './nav.css'
 import { FaSearch } from 'react-icons/fa';
-import { MdPersonOutline, MdOutlineShoppingCart } from "react-icons/md";
+import { MdPersonOutline, MdOutlineShoppingCart, MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { BsPersonCheck } from "react-icons/bs";
 import NavBottom from './navBottom';
 import { useSelector } from 'react-redux';
 
 
 const Nav = () => {
     const cartData = useSelector(state => state.cart)
+    const userData = useSelector(state => state.user.user)
+    const userFirstName = userData.user.name.split(' ')
+    console.log(" user first name:" ,userFirstName);
+    
+
 
     return (
         <>
@@ -23,14 +29,31 @@ const Nav = () => {
                     </div>
                     <div className='nav_top_right'>
                         <div className='account'>
-                            <Link to='/signup' className='cart_link'><button><MdPersonOutline className='account_icon' /> Sign In</button></Link>
+                            {userData ? (
+                                <div className='account_info'>
+                                    <input type='checkbox'></input>
+                                    <label><span><BsPersonCheck /></span>Hi, {userFirstName} <MdOutlineKeyboardArrowDown /></label>
+                                    <div className='account_box'>
+                                        <div className='box'>
+                                            <Link to='/profile' className='cart_link'><p className='account_text'>My account</p></Link>
+                                            <Link to='/' className='cart_link'><p className='account_text'>Saved items</p></Link>
+                                            <form method='POST' action='/logout'>
+                                                <button type='submit'>Log out</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                    {/* <button><BsPersonCheck className='account_icon' /> Hi, {userData.user.name}</button> */}
+                                </div>
+                            ) : (
+                                <Link to='/signup' className='cart_link'><button><MdPersonOutline className='account_icon' /> Sign In</button></Link>
+                            )}
                         </div>
                         <div className='cart_btn'>
                             <Link to='/cart' className='cart_link'><button><MdOutlineShoppingCart className='cart_icon' /> Cart {cartData.number > 0 && <span><sup>({cartData.number})</sup></span>}</button></Link>
                         </div>
                     </div>
                 </div>
-                <hr></hr>
+                <hr />
                 <NavBottom />
             </div>
         </>
